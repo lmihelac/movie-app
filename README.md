4a9c3a343e7b64befbcb3f87cb724aeccb645929
Luka Mihelac <luka.mihelac7@gmail.com>
2021-03-30 20:32:45 +0200

    Added last things to project functionality, but will be planing to do some more

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # movie-app
  Web Application with a list of movies from external database
  Containes movie info, possibility to add favorites
+
+
+
+
+ 30.03.2021. ---> Will be laying off this project for some time since I have some more things
+ to learn.
--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const search = document.querySelector('search');
 
-async function getMovies() {
-    const resp = await fetch(APIURL);
-    const respData = await resp.json();
-
-    console.log(respData);
-
-    respData.results.forEach(movie => {
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-
-        movieEl.innerHTML = `
-
-        <img src="${}" alt="${}">
-        <div class="movie-info">
-            <h3>${}</h3>
-            <span>${}</span>
-        </div>
-        `
-
-    })
-
-
-    return respData;
-}
 
 getMovies();
 
-get api json.fetch() if(11 && 22)
+
+async function getMovies() {
+  const resp = await fetch(APIURL);
+  const respData = await resp.json();
+
+  console.log(respData);
+
+  respData.results.forEach((movie) => {
+    const{ poster_path, title, vote_average}
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+
+        <img 
+        src="${IMGPATH + poster_path}" 
+        alt="${title}"/>
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+
+        `;
+
+        main.appendChild(movieEl);
+  });
+
+  return respData;
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red';
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value
+})
 
